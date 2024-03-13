3b1abc01a5bd8ebd39f16bc7964c004b053115c8
vedant1711 <vscode1711@gmai.com>
2024-03-13 14:38:25 +0530

    added team_name to the admin panel of the APIhit

--- hackathon_api/hackathon/admin.py
+++ hackathon_api/hackathon/admin.py
@@ -3,8 +3,13 @@ from .models import Team, ApiHitCount
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('team_name', 'access_key')
+    list_display = ('id', 'team_name', 'access_key')
 
 @admin.register(ApiHitCount)
 class ApiHitCountAdmin(admin.ModelAdmin):
-    list_display = ('team', 'api_code', 'time_refreshed', 'api_hits', 'timestamp')
+    list_display = ('id','team_name', 'api_code', 'time_refreshed', 'api_hits', 'timestamp')
+
+    def team_name(self, obj):
+        return obj.team.team_name
+
+    team_name.admin_order_field = 'team__team_name'
